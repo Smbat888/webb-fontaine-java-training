439330a0e5ea44242e2264fbc4a91982f731f6b7
samvelmkrtchyan93 <55761883+samvelmkrtchyan93@users.noreply.github.com>
2019-11-12 11:06:41 +0400

    Add files via upload

---
+++ Samvel/Library/DataLibrary.java
@@ -0,0 +1,9 @@
+public class DataLibrary {
+
+    public static String[] names = {"Anjey", "George", "Joan", "Steven", "Hovhannes", "Lev"};
+    public static String[] surnames = {"Sapovski", "Martin", "Rolling", "King", "Tumanyan", "Tolstoy"};
+    public static int[] ages = {65, 70, 48, 55, 120, 200};
+
+    public static String[] titles = {"The Withcer", "GOT", "Harry Potter", "MIST", "Tmkaberdi Arum", "Voyna i Mir"};
+
+}
