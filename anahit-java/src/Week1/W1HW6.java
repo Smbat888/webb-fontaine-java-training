218a8163c744ee486663b2967be729575a5c9ebe
Anahit Tumanyan <tumanyananahit1@gmail.com>
2019-11-27 17:09:40 +0400

    Week 1 homework

--- anahit-java/src/Week1/W1HW6.java
+++ anahit-java/src/Week1/W1HW6.java
@@ -1,31 +1,30 @@
-package Week1;
+package src.Week1;
 
 import java.util.Scanner;
 
 public class W1HW6 {
 
     public static void main(String[] args) {
+        int x = 3;
+        System.out.println(getneristPrimeNumber(x));
+    }
 
-        Scanner s = new Scanner(System.in);
-        System.out.println("please enter a number");
-        boolean isPrime = true;
-        int mnatsord;
-        int x = s.nextInt();
-        if (x == 0 || x == 1) {
-
-            System.out.println(x + " is not a prime number");
-            return;
-        }
-
-        for (int i = 2; i <= x / 2; i++) {
-            mnatsord = x % i;
-
-            if (mnatsord == 0) {
-                isPrime = false;
-                break;
+    public static boolean isPrime(int number){
+        for(int i =2; i< number/2; i++){
+            if(number%i==0) {
+                return false;
             }
         }
+        return true;
+    }
 
+    public static int  getneristPrimeNumber(int number){
+        do {
+            if (isPrime(number)) {
+                return number;
+            }
+            number++;
+        }while (true);
 
     }
 }
