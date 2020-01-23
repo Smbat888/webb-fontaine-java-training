c654e69376dfbdb7324d0138e70b9e864d3c7271
Hmayak Atayan <hmayak.atayan@gmail.com>
2020-01-23 11:12:53 +0400

    add  users object and pass parameter from the json

--- Hamo/src/week6/GetJsonDataFromNetwork.java
+++ Hamo/src/week6/GetJsonDataFromNetwork.java
@@ -1,5 +1,7 @@
 package week6;
 
+import com.google.gson.Gson;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,17 +16,19 @@ import java.net.UnknownHostException;
  * * Save it in local file. +
  * * Each time when program starts: check the internet availability(online/offline) +
  * * Read json from cache (saved local file) when offline and get from the internet when online (also, save again in the same local file).+
- * * Convert the json array/object to Java models and print (GSON) -------
+ * * Convert the json array/object to Java models and print (GSON) +
  * * the url should be used from property file +
  */
 public class GetJsonDataFromNetwork {
     private static String json;
-    private static String jsonFilePath = "Hamo/resource/jsonForHamo.txt";
+    private static String jsonFilePath = "Hamo/resource/jsonForHamo.json";
 
     public static void main(String[] args) {
         jsonFromNetwork();
 
         FileWriter.writeIntoFile(json, jsonFilePath);
+
+        initObjectsAsParameterFromJson();
     }
 
     public static void ping() {
@@ -63,23 +67,18 @@ public class GetJsonDataFromNetwork {
             json = sb.toString();
 
         } catch (UnknownHostException internetChka) {
-            FileReader.readFile(jsonFilePath);
+            json = FileReader.readFile(jsonFilePath);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
-    //todo check this part
-//
-//            System.out.println("json = " + json);
-////
-////            JSONArray jsonArray = new JSONArray(json);
-////            for (int i = 0; i < jsonArray.length(); ++i) {
-////                JSONObject jsonUser = jsonArray.getJSONObject(i);
-////
-////                Gson gson = new Gson();
-////                UsersArray user = gson.fromJson(json, UsersArray.class);
-////                System.out.println("======================");
-////            }
+    public static void initObjectsAsParameterFromJson() {
+        Gson gson = new Gson();
+        User[] users = gson.fromJson(json, User[].class);
+        for (User user : users) {
+            System.out.println(user);
+        }
+    }
 }
--- Hamo/src/week6/User.java
+++ Hamo/src/week6/User.java
@@ -7,4 +7,20 @@ public class User {
     public Integer id;
     public String title;
     public Boolean completed;
+
+    public User() {
+    }
+
+    public User(Integer userId, Integer id, String title, Boolean completed) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("user: %d , id: %d , title: %s , completed: %s", userId, id, title, completed);
+    }
 }
